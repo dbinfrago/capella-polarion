25b1c70cac0dbaa562c9f016f0a07a14c1422c5c
ernstwuerger <ernst.wuerger@deutschebahn.com>
2026-01-06 11:03:52 +0100

    ci: Fix docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@ import sys
 sys.path.insert(0, os.path.abspath("../.."))
 
 # -- Project information -----------------------------------------------------
-import tomli as tomllib
+import tomllib
 
 import capella2polarion
 
